9ca4dc305b80433cc76869644ce09d1f96e9f529
tranthienbinh1989 <tranthienbinh1989@gmail.com>
2016-08-28 15:04:43 -0500

    add user repository

--- src/com/mum/carpooling/model/User.java
+++ src/com/mum/carpooling/model/User.java
@@ -2,7 +2,7 @@ package com.mum.carpooling.model;
 
 import java.sql.*;
 
-public class User{
+public class User {
 	private long userid;
 	private String fullname;
 	private boolean gender;
@@ -15,84 +15,108 @@ public class User{
 	private String password;
 	private Timestamp datecreated;
 	private Timestamp dateupdated;
-	
+
 	public long getUserid() {
 		return userid;
 	}
+
 	public void setUserid(long userid) {
 		this.userid = userid;
 	}
+
 	public String getFullname() {
 		return fullname;
 	}
+
 	public void setFullname(String fullname) {
 		this.fullname = fullname;
 	}
+
 	public boolean isGender() {
 		return gender;
 	}
+
 	public void setGender(boolean gender) {
 		this.gender = gender;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getStreet() {
 		return street;
 	}
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
+
 	public int getZipcode() {
 		return zipcode;
 	}
+
 	public void setZipcode(int zipcode) {
 		this.zipcode = zipcode;
 	}
+
 	public int getBirthyear() {
 		return birthyear;
 	}
+
 	public void setBirthyear(int birthyear) {
 		this.birthyear = birthyear;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Timestamp getDatecreated() {
 		return datecreated;
 	}
+
 	public void setDatecreated(Timestamp datecreated) {
 		this.datecreated = datecreated;
 	}
+
 	public Timestamp getDateupdated() {
 		return dateupdated;
 	}
+
 	public void setDateupdated(Timestamp dateupdated) {
 		this.dateupdated = dateupdated;
 	}
-	
-	public static boolean saveNewUser(User user){
+
+	public static boolean saveNewUser(User user) {
 		return false;
 	}
-	public static boolean editUser(String userid, User user){
+
+	public static boolean editUser(String userid, User user) {
 		return false;
 	}
 }
--- src/com/mum/carpooling/ultil/DBConnection.java
+++ src/com/mum/carpooling/ultil/DBConnection.java
@@ -1,66 +1,35 @@
 package com.mum.carpooling.ultil;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Properties;
 
-/**
- *
- * @author Asaad Saad
- * @25 Aug 2016 A very simple example of JDBC connection to a MySQL
- * database. Uses same technique taught in the FPP course. Does not do any
- * connection management or pooling, just opens and closes the connection for
- * each request.
- */
 public class DBConnection {
+	private static Connection dbConnection = null;
 
-    String dburl = "jdbc:mysql://localhost:3306/carpoolingdb";
+	public static Connection getConnection() {
+		if (dbConnection != null) {
+			return dbConnection;
+		} else {
+			try {
+				InputStream inputStream = DBConnection.class.getClassLoader().getResourceAsStream("db.properties");
+				Properties properties = new Properties();
+				if (properties != null) {
+					properties.load(inputStream);
 
-    public DBConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException ex) {
-            System.out.println("MySQL JDBC driver not found in DBConnection\n" + ex);
-            System.exit(0);
-        }
-    }
+					String dbDriver = properties.getProperty("dbDriver");
+					String connectionUrl = properties.getProperty("connectionUrl");
+					String userName = properties.getProperty("userName");
+					String password = properties.getProperty("password");
 
-    String retrieveUserFullname(String email) {
-        String readQuery = "SELECT fullname from users where email = '" + email + "';";
-        String fullname = "No information found for the requested user: " + email;
-
-        try (Connection con = DriverManager.getConnection(dburl, "root", "mumsql");
-                Statement stmt = con.createStatement();) {
-
-            System.out.println("the query: " + readQuery);
-            ResultSet rs = stmt.executeQuery(readQuery);
-            while (rs.next()) {
-                fullname = rs.getString("fullname");
-                System.out.println("User Fullname: " + fullname);
-            }
-            stmt.close();
-
-        } catch (SQLException s) {
-            System.out.println("Exception thrown in retrieveUser ....");
-            s.printStackTrace();
-        }
-
-        return fullname;
-
-    }
-
-    public String mockRetrieveUserFullname(String email) {
-        String fullname = "no definition found";
-        switch (email) {
-            case "asaad@mum.edu":
-                fullname = "Asaad Saad";
-                break;
-            default: fullname = "CS472 Student";
-                break;
-        }
-        return fullname;
-    }
-
+					Class.forName(dbDriver).newInstance();
+					dbConnection = DriverManager.getConnection(connectionUrl, userName, password);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return dbConnection;
+		}
+	}
 }
