77425b0df99caa084327352914ce4e50314a014c
esaaqo <esaaq@gmail.com>
2016-08-28 14:33:38 -0500

    Initial User Classes

---
+++ src/com/carpool/model/UserSession.java
@@ -0,0 +1,14 @@
+package com.carpool.model;
+
+public class UserSession {
+	private User user;
+	public UserSession(User user){
+		this.user = user;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
