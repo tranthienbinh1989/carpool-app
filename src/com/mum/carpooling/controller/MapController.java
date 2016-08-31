bd7457af7f08c6b5f94d3059e4fba08a433f969f
tranthienbinh1989 <tranthienbinh1989@gmail.com>
2016-08-31 14:51:22 -0500

    finish ridemap

--- src/com/mum/carpooling/controller/MapController.java
+++ src/com/mum/carpooling/controller/MapController.java
@@ -46,7 +46,7 @@ public class MapController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String Action = request.getParameter("Action");
+		String Action = request.getParameter("action");
 		if(Action!=null && !Action.isEmpty()){
 			if(Action.equals("getFrom")){
 				String fromLat = request.getParameter("fromLat");
@@ -58,6 +58,7 @@ public class MapController extends HttpServlet {
 						JSONObject p = new JSONObject();
 						p.put("postId", post.getPostid());
 						p.put("postType", post.getPosttype());
+						p.put("postContent", post.getPost());
 						p.put("fullname", post.getFullname());
 						p.put("fromLat", post.getFromLatitue());
 						p.put("fromLong", post.getFromLongitue());
@@ -69,7 +70,7 @@ public class MapController extends HttpServlet {
 					
 			} else {
 				response.setContentType("application/json");
-				response.getWriter().write("Wrong action");
+				response.getWriter().write("{}");
 			}
 		}
 	}
